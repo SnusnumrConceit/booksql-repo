67a7f47f5dc7e0a76d4ccabee19415a5f4e81c97
snusnumr1 <snusnumr1@gmail.com>
2019-08-23 17:40:00 +0400

    BGQL-3: удалены мутации для пользователей из конфига; добавлены мутации для категорий в конфиг

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+	public $fillable = ['name'];
+	
     public function books() : HasMany
     {
     	return $this->hasMany(Book::class);
